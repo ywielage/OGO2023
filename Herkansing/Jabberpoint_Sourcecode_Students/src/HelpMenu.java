57cf9511466c7b63f311cf8596783327acd4e242
ywielage <32356416+ywielage@users.noreply.github.com>
2023-04-02 21:55:23 +0200

    Push

--- Herkansing/Jabberpoint_Sourcecode_Students/src/HelpMenu.java
+++ Herkansing/Jabberpoint_Sourcecode_Students/src/HelpMenu.java
@@ -9,24 +9,6 @@ public class HelpMenu extends MenuOption {
 
     MenuItem menuItem;
     protected static final String ABOUT = "About";
-    protected static final String FILE = "File";
-    protected static final String EXIT = "Exit";
-    protected static final String GOTO = "Go to";
-    protected static final String HELP = "Help";
-    protected static final String NEW = "New";
-    protected static final String NEXT = "Next";
-    protected static final String OPEN = "Open";
-    protected static final String PAGENR = "Page number?";
-    protected static final String PREV = "Prev";
-    protected static final String SAVE = "Save";
-    protected static final String VIEW = "View";
-
-    protected static final String TESTFILE = "testPresentation.xml";
-    protected static final String SAVEFILE = "savedPresentation.xml";
-
-    protected static final String IOEX = "IO Exception: ";
-    protected static final String LOADERR = "Load Error";
-    protected static final String SAVEERR = "Save Error";
 
     public HelpMenu(Presentation presentation, Frame parent, Menu menu){
         super(presentation, parent, menu);
--- Herkansing/Jabberpoint_Sourcecode_Students/src/MenuController.java
+++ Herkansing/Jabberpoint_Sourcecode_Students/src/MenuController.java
@@ -24,26 +24,13 @@ public class MenuController extends MenuBar {
 	private Presentation presentation; //Commands are given to the presentation
 	
 	private static final long serialVersionUID = 227L;
-	
-	protected static final String ABOUT = "About";
 	protected static final String FILE = "File";
-	protected static final String EXIT = "Exit";
-	protected static final String GOTO = "Go to";
 	protected static final String HELP = "Help";
-	protected static final String NEW = "New";
-	protected static final String NEXT = "Next";
-	protected static final String OPEN = "Open";
-	protected static final String PAGENR = "Page number?";
-	protected static final String PREV = "Prev";
-	protected static final String SAVE = "Save";
 	protected static final String VIEW = "View";
-	
 	protected static final String TESTFILE = "testPresentation.xml";
-	protected static final String SAVEFILE = "savedPresentation.xml";
-	
 	protected static final String IOEX = "IO Exception: ";
 	protected static final String LOADERR = "Load Error";
-	protected static final String SAVEERR = "Save Error";
+
 
 	public MenuController(Frame frame, Presentation pres) {
 		parent = frame;
